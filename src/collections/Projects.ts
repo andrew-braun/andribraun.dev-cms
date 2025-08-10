85c678bbb7aa63f24036394d6dc7d1f80767515e
andrew-braun <andrew@andrewbraun.dev>
2025-08-10 17:53:33 +0700

    Initial commit with basic Payload setup

---
+++ src/collections/Projects.ts
@@ -0,0 +1,41 @@
+import type { CollectionConfig } from 'payload'
+
+export const Projects: CollectionConfig = {
+  slug: 'projects',
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+    },
+    {
+      name: 'description',
+      type: 'textarea',
+    },
+    {
+      label: 'links',
+      type: 'collapsible',
+      fields: [
+        {
+          name: 'liveLink',
+          type: 'text',
+        },
+        {
+          name: 'snapshotLink',
+          type: 'text',
+        },
+        { name: 'githubLink', type: 'text' },
+      ],
+    },
+    {
+      label: 'media',
+      type: 'collapsible',
+      fields: [
+        {
+          name: 'images',
+          type: 'upload',
+          relationTo: 'media',
+        },
+      ],
+    },
+  ],
+}

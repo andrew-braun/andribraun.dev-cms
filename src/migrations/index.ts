75c1485b5ebb4c8afa84ae96352775bb2ddbb062
Andri Braun <andrew@andrewbraun.dev>
2026-01-18 14:59:12 +0700

    feat: Create payload migration file

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260118_075847 from './20260118_075847';
+
+export const migrations = [
+  {
+    up: migration_20260118_075847.up,
+    down: migration_20260118_075847.down,
+    name: '20260118_075847'
+  },
+];

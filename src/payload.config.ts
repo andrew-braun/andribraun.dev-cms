851005d2480321aff3c71a47c9183958f1dc4510
Andri Braun <andrew@andrewbraun.dev>
2026-01-20 23:03:48 +0700

    feat-new: Add Cloudflare R2 as S3 upload provider

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,12 +1,13 @@
 // storage-adapter-import-placeholder
+import { S3Client } from '@aws-sdk/client-s3'
 import { postgresAdapter } from '@payloadcms/db-postgres'
 import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import { s3Storage } from '@payloadcms/storage-s3'
 import path from 'path'
 import { buildConfig } from 'payload'
 import sharp from 'sharp'
 import { fileURLToPath } from 'url'
-
 import { Media } from './collections/Media'
 import { Projects } from './collections/Projects'
 import { Tags } from './collections/Tags'
@@ -37,6 +38,21 @@ export default buildConfig({
   sharp,
   plugins: [
     payloadCloudPlugin(),
+    s3Storage({
+      collections: {
+        media: true,
+      },
+      bucket: process.env.R2_BUCKET_NAME || '',
+      config: {
+        region: 'auto',
+        endpoint: process.env.R2_ENDPOINT,
+        credentials: {
+          accessKeyId: process.env.R2_ACCESS_KEY_ID!,
+          secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!,
+        },
+        forcePathStyle: true,
+      },
+    }),
     // storage-adapter-placeholder
   ],
 })

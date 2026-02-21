934496323e07cad89bef304b59ccac8028c3929c
Andri Braun <andrew@andrewbraun.dev>
2026-02-21 15:03:31 +0700

    chore: Attempt migrations (plugin not working

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,15 +1,21 @@
-import * as migration_20260118_075847 from './20260118_075847';
-import * as migration_20260120_144049 from './20260120_144049';
+import * as migration_20260118_075847 from './20260118_075847'
+import * as migration_20260120_144049 from './20260120_144049'
+import * as migration_20260221_073939 from './20260221_073939'
 
 export const migrations = [
   {
-    up: migration_20260118_075847.up,
-    down: migration_20260118_075847.down,
     name: '20260118_075847',
+    down: migration_20260118_075847.down,
+    up: migration_20260118_075847.up,
   },
   {
-    up: migration_20260120_144049.up,
+    name: '20260120_144049',
     down: migration_20260120_144049.down,
-    name: '20260120_144049'
+    up: migration_20260120_144049.up,
   },
-];
+  {
+    name: '20260221_073939',
+    down: migration_20260221_073939.down,
+    up: migration_20260221_073939.up,
+  },
+]
